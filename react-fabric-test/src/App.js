9264665729930b1d4841db58eebb5cb1d61314ec
naufalwibs <naufalwibisono95@gmail.com>
2021-04-16 20:37:09 +0700

    T-shirt Done

--- react-fabric-test/src/App.js
+++ react-fabric-test/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
 import tShirt from "./assets/background_tshirt.png";
+import domtoimage from "dom-to-image";
+import batman from "./assets/batman.png";
 
 const App = () => {
-  const imgUrlBatman =
-    "https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5b0e95d294c.png";
+  // const imgUrlBatman =
+  //   "https://cdn.ourcodeworld.com/public-media/gallery/gallery-5d5b0e95d294c.png"; || Kena Cors
   const [canvas, setCanvas] = useState("");
   const [tShirtColor, setTshirtColor] = useState("#fff");
+  const [refTshirt] = useState(React.createRef());
+  const [createdImage, setCreatedImage] = useState("");
 
   useEffect(() => {
     setCanvas(tShirtCanvas());
@@ -18,8 +22,8 @@ const App = () => {
       width: 200,
     });
 
-  const addRect = (canvi) => {
-    fabric.Image.fromURL(imgUrlBatman, function (myImg) {
+  const addMainLogo = (canvi) => {
+    fabric.Image.fromURL(batman, function (myImg) {
       myImg.scaleToHeight(200);
       myImg.scaleToWidth(200);
       canvi.centerObject(myImg);
@@ -29,36 +33,63 @@ const App = () => {
   };
 
   const changeColor = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     setTshirtColor(e.target.value);
+  };
+
+  const node = refTshirt.current;
+
+  const saveToImage = (e) => {
+    // console.log(e);
+    domtoimage
+      .toPng(node)
+      .then(function (dataUrl) {
+        // Print the data URL of the picture in the Console
+        console.log(dataUrl);
+
+        // You can for example to test, add the image at the end of the document
+        let img = new Image();
+        img.src = dataUrl;
+        img.crossOrigin = "anonymous";
+        setCreatedImage(dataUrl);
+        // document.body.appendChild(document.createElement("DIV"));
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
   };
 
   return (
     <>
-      <div id="tshirt-div" style={{ backgroundColor: tShirtColor }}>
+      <div
+        ref={refTshirt}
+        id="tshirt-div"
+        style={{ backgroundColor: tShirtColor }}
+      >
         <img id="tshirt-backgroundpicture" src={tShirt} alt="..." />
-        <button onClick={() => addRect(canvas)}>Logo</button>
         <div id="drawingArea" className="drawing-area">
           <div className="canvas-container">
             <canvas id="tshirt-canvas"></canvas>
           </div>
         </div>
-        <div>
-          <label>T-Shirt Color:</label>
-          <select onChange={changeColor} id="tshirt-color">
-            <option value="#fff">White</option>
-            <option value="#000">Black</option>
-            <option value="#f00">Red</option>
-            <option value="#008000">Green</option>
-            <option value="#ff0">Yellow</option>
-          </select>
-        </div>
-        <div>
-          <label>Upload your own design:</label>
-          <input type="file" id="tshirt-custompicture" />
-          <button>Save Change</button>
-        </div>
       </div>
+      <div>
+        <button onClick={() => addMainLogo(canvas)}>Logo</button>
+        <label>T-Shirt Color:</label>
+        <select onChange={changeColor} id="tshirt-color">
+          <option value="#fff">White</option>
+          <option value="#000">Black</option>
+          <option value="#f00">Red</option>
+          <option value="#008000">Green</option>
+          <option value="#ff0">Yellow</option>
+        </select>
+      </div>
+      <div>
+        {/* <label>Upload your own design:</label> */}
+        {/* <input type="file" id="tshirt-custompicture" /> */}
+        <button onClick={saveToImage}>Save Change</button>
+      </div>
+      <div>{createdImage ? <img src={createdImage} alt="..." /> : ""}</div>
     </>
   );
 };
